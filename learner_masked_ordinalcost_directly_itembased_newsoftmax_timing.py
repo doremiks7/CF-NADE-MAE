771540464a57a8043c002407806f3e09f9246091
truong <anhthaiw125@gmail.com>
2018-04-21 23:57:43 +0700

    first commit

--- learner_masked_ordinalcost_directly_itembased_newsoftmax_timing.py
+++ learner_masked_ordinalcost_directly_itembased_newsoftmax_timing.py
@@ -755,6 +755,7 @@ if __name__ == '__main__':
         print ('\tValidating ...',)
         start_time = t.time()
         squared_error_valid = []
+        absolute_error_valid - []
         n_sample_valid = []
         valid_time = 0
         for batch in valid_monitor_stream.get_epoch_iterator():
@@ -824,6 +825,7 @@ if __name__ == '__main__':
     print ('\tTesting ...',)
     start_time = t.time()
     squared_error_test = []
+    absolute_error_test = []
     n_sample_test = []
     test_time = 0
     for batch in test_monitor_stream.get_epoch_iterator():
